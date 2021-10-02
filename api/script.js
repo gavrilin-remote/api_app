38ba9f19a973ae59351028101de1f08f9b4d4cb1
Andrew <rtpdfv@gmail.com>
2021-10-02 20:47:29 +0300

    .

--- api/script.js
+++ api/script.js
@@ -1,23 +1,21 @@
-const mysql = require('mysql');
-const express = require('express');
-const bodyparser = require('body-parser');
+const mysql = require("mysql");
+const express = require("express");
+const bodyparser = require("body-parser");
 var app = express();
 //Configuring express server
 app.use(bodyparser.json());
 
 var mysqlConnection = mysql.createConnection({
-host: 'localhost',
-user: 'andrew',
-password: 'Qwerty!23',
-database: 'learnerdetails',
-multipleStatements: true
+	host: "localhost",
+	user: "andrew",
+	password: "Qwerty!23",
+	database: "learnerdetails",
+	multipleStatements: true,
 });
 
-mysqlConnection.connect((err)=> {
-if(!err)
-console.log('Connection Established Successfully');
-else
-console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
+mysqlConnection.connect((err) => {
+	if (!err) console.log("Connection Established Successfully");
+	else console.log("Connection Failed!" + JSON.stringify(err, undefined, 2));
 });
 
 //Establish the server connection
@@ -25,56 +23,65 @@ console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
 
-app.get('/learners' , (req, res) => {
-mysqlConnection.query('SELECT * FROM learnerdetails', (err, rows, fields) => {
-if (!err)
-res.send(rows);
-else
-console.log(err);
-})
+app.get("/learners", (req, res) => {
+	mysqlConnection.query("SELECT * FROM learnerdetails", (err, rows, fields) => {
+		if (!err) res.send(rows);
+		else console.log(err);
+	});
 });
-
 
 //Router to GET specific learner detail from the MySQL database
-app.get('/learners/:id' , (req, res) => {
-mysqlConnection.query('SELECT * FROM learnerdetails WHERE learner_id = ?',[req.params.id], (err, rows, fields) => {
-if (!err)
-res.send(rows);
-else
-console.log(err);
-})
+app.get("/learners/:id", (req, res) => {
+	mysqlConnection.query(
+		"SELECT * FROM learnerdetails WHERE learner_id = ?",
+		[req.params.id],
+		(err, rows, fields) => {
+			if (!err) res.send(rows);
+			else console.log(err);
+		}
+	);
 });
-
-
 
 //Router to INSERT/POST a learner's detail
-app.post('/learners', (req, res) => {
-let learner = req.body;
-var sql = "SET @learner_id = ?;SET @learner_name = ?;SET @learner_email = ?;SET @course_Id = ?; CALL learnerAddOrEdit(@learner_id,@learner_name,@learner_email,@course_Id);";
-mysqlConnection.query(sql, [learner.learner_id, learner.learner_name, learner.learner_email, learner.course_Id], (err, rows, fields) => {
-if (!err)
-rows.forEach(element => {
-if(element.constructor == Array)
-res.send('New Learner ID : '+ element[0].learner_id);
+app.post("/learners", (req, res) => {
+	let learner = req.body;
+	var sql =
+		"SET @learner_id = ?;SET @learner_name = ?;SET @learner_email = ?;SET @course_Id = ?; CALL learnerAddOrEdit(@learner_id,@learner_name,@learner_email,@course_Id);";
+	mysqlConnection.query(
+		sql,
+		[
+			learner.learner_id,
+			learner.learner_name,
+			learner.learner_email,
+			learner.course_Id,
+		],
+		(err, rows, fields) => {
+			if (!err)
+				rows.forEach((element) => {
+					if (element.constructor == Array)
+						res.send("New Learner ID : " + element[0].learner_id);
+				});
+			else console.log(err);
+		}
+	);
 });
-else
-console.log(err);
-})
-});
-
-
-
 
 //Router to UPDATE a learner's detail
-app.put('/learners', (req, res) => {
-let learner = req.body;
-var sql = "SET @learner_id = ?;SET @learner_name = ?;SET @learner_email = ?;SET @course_Id = ?; CALL learnerAddOrEdit(@learner_id,@learner_name,@learner_email,@course_Id);";
-mysqlConnection.query(sql, [learner.learner_id, learner.learner_name, learner.learner_email, learner.course_Id], (err, rows, fields) => {
-if (!err)
-res.send('Learner Details Updated Successfully');
-else
-console.log(err);
-})
+app.put("/learners", (req, res) => {
+	let learner = req.body;
+	var sql =
+		"SET @learner_id = ?;SET @learner_name = ?;SET @learner_email = ?;SET @course_Id = ?; CALL learnerAddOrEdit(@learner_id,@learner_name,@learner_email,@course_Id);";
+	mysqlConnection.query(
+		sql,
+		[
+			learner.learner_id,
+			learner.learner_name,
+			learner.learner_email,
+			learner.course_Id,
+		],
+		(err, rows, fields) => {
+			if (!err) res.send("Learner Details Updated Successfully");
+			else console.log(err);
+		}
+	);
 });
-
-
